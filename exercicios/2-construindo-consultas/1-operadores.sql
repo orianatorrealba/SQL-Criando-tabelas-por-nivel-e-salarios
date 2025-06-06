55854c975ea6fbb1697065f9d6f44a268e10f601
orianatorrealba <oriana.torrealba@viavarejo.com.br>
2025-06-06 00:44:36 +0000

    Criando consultas com operadores logicos

--- exercicios/2-construindo-consultas/1-operadores.sql
+++ exercicios/2-construindo-consultas/1-operadores.sql
@@ -1,6 +1,25 @@
 -- Construa uma consulta para a tabela "invoice_items" que retorne apenas as colunas InvoiceId, TrackId e UnitPrice.
-
+SELECT
+TrackID,
+UnitPrice
+FROM
+invoice_items;
 -- Inclua nessa consulta mais três colunas, cada uma como resultado de uma operação aritmética diferente
-
+SELECT
+TrackID,
+UnitPrice,
+UnitPrice * 1.3,
+UnitPrice + 2.11,
+(UnitPrice + 2.11) % 2
+FROM 
+invoice_items;
 -- Incremente essa consulta utilizando a cláusula WHERE, a qual restringirá o resultado apenas para linhas cujo o InvoiceId seja maior do que 20 e menor ou igual a 30
-
+SELECT
+TrackID,
+UnitPrice,
+UnitPrice * 1.3,
+UnitPrice + 2.11,
+(UnitPrice + 2.11) % 2
+FROM invoice_items
+WHERE InvoiceId > 20
+AND InvoiceId <= 30;
--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,60 @@
 -- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
-
+SELECT DISTINCT
+CustomerId as ID,
+FirstName as Nome,
+Lastname as Sobrenome,
+Address as Endereço
+FROM customers
+WHERE
+Country LIKE 'Brazil'; 
 
 -- Na consulta anterior, converta o sobrenome para letras maiúsculas
-
+SELECT DISTINCT
+CustomerId as ID,
+FirstName as Nome,
+UPPER (Lastname) as Sobrenome,
+Address as Endereço
+FROM customers
+WHERE
+Country LIKE 'Brazil'; 
 
 -- Na consulta anterior, converta o nome para letras minúsculas
-
+SELECT DISTINCT
+CustomerId as ID,
+LOWER (FirstName) as Nome,
+LastName as Sobrenome,
+Address as Endereço
+FROM customers
+WHERE
+Country LIKE 'Brazil'; 
 
 -- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
-
+SELECT DISTINCT
+CustomerId as ID,
+FirstName as Nome,
+LastName as Sobrenome,
+FirstName || ' ' || LastName as Nome_completo,
+FROM customers
+WHERE 
+Country LIKE 'Brazil';
 
 -- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
-
+SELECT DISTINCT
+CustomerId as ID,
+FirstName as Nome,
+Lastname as Sobrenome,
+Address as Endereço,
+REPLACE(Address, 'Av.','Avenida') as endereço
+FROM customers
+WHERE
+Country LIKE 'Brazil'; 
 
 -- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
+SELECT DISTINCT
+CustomerId as ID,
+FirstName as Nome,
+Lastname as Sobrenome,
+Address as Endereço
+FROM customers
+WHERE
+Country LIKE 'Bra%'; 

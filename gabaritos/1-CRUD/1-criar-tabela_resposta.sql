9ec6790c6f1a7b8d675c7a473e68b686458ecb6a
orianatorrealba <oriana.torrealba@viavarejo.com.br>
2025-06-11 22:18:33 +0000

    SQL na pratica

--- gabaritos/1-CRUD/1-criar-tabela_resposta.sql
+++
@@ -1,6 +0,0 @@
--- Crie uma nova tabela no banco de dados
-CREATE TABLE salario_classe (
-  id_salario INTEGER PRIMARY KEY AUTOINCREMENT,
-  nivel TEXT,
-  salario INTEGER
-);
--- gabaritos/3-consultando-dados-varias-tabelas/1-tipos-joins_resposta.sql
+++
@@ -1,4 +0,0 @@
--- Crie uma consulta para realizar um left join com as tabelas "albums" e "artists"
-SELECT *
-FROM albums
-LEFT JOIN artists ON albums.ArtistId = artists.ArtistId;
